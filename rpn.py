7ea6b540c49a088310eeb11780c4f964372d9b7a
Anthony Wohlfeil <antwohlf@umich.edu>
2019-11-06 21:12:59 -0500

    implement subtraction and error handling

--- rpn.py
+++ rpn.py
@@ -6,10 +6,18 @@ def calculate(arg):
         if token == '+':
             arg1 = stack.pop()
             arg2 = stack.pop()
-            result = arg1 + arg2
+            result = arg2 + arg1
+            stack.append(result)
+        elif token == '-':
+            arg1 = stack.pop()
+            arg2 = stack.pop()
+            result = arg2 - arg1
             stack.append(result)
         else:
             stack.append(int(token))
+
+    if len(stack) != 1:
+        raise TypeError('malformed input')
     return stack.pop()
 
 def main():
