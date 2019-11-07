969f16e0df3b247683a6ca5fd32f154cdd571981
Anthony Wohlfeil <antwohlf@umich.edu>
2019-11-06 21:43:43 -0500

    implemented division

--- test_rpn.py
+++ test_rpn.py
@@ -11,6 +11,9 @@ class TestBasics(unittest.TestCase):
     def test_mul(self):
         result = rpn.calculate("3 2 *")
         self.assertEqual(6, result)
+    def test_div(self):
+        result = rpn.calculate("6 2 /")
+        self.assertEqual(3, result)
     def test_badinput(self):
         with self.assertRaises(TypeError):
             rpn.calculate(' 1 2 3 +')
