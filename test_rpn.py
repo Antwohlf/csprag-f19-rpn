92c925ad1f6e515f881f5464796bf78e47623985
Anthony Wohlfeil <antwohlf@umich.edu>
2019-11-06 21:48:14 -0500

    implemented exponential

--- test_rpn.py
+++ test_rpn.py
@@ -14,6 +14,9 @@ class TestBasics(unittest.TestCase):
     def test_div(self):
         result = rpn.calculate("6 2 /")
         self.assertEqual(3, result)
+    def test_exp(self):
+        result = rpn.calculate("3 2 ^")
+        self.assertEqual(9, result)
     def test_badinput(self):
         with self.assertRaises(TypeError):
             rpn.calculate(' 1 2 3 +')
